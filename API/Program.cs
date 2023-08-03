58636c96374bba4bdafdf47ea1004b7a6db52696
Pankaj <pankajbca11@gmail.com>
2023-08-03 06:29:43 +0530

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -5,10 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
 
 
 var app = builder.Build();
@@ -16,5 +18,6 @@ var app = builder.Build();
 //Configure the HTTP request pipeline.
 
 app.MapControllers();
+app.UseCors(builder =>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.Run();
